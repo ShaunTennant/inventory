23f9edff004a36231d69cf9ed228d8a665c64b48
ShaunTennant <shauntennant@bigpond.com>
2015-11-13 18:41:33 +0800

    still has input name issues

--- src/components/inventoryfilters/inventoryfilters.component.ts
+++ src/components/inventoryfilters/inventoryfilters.component.ts
@@ -1,12 +1,12 @@
 import { Component, View, NgFor, NgClass } from 'angular2/angular2';
-import { Inventory, InventoryItem, Key, Count } from '../inventoryshared/inventorytable.class'
+import { Inventory, InventoryItem, Key, Count, Test } from '../inventoryshared/inventorytable.class'
 
 @Component({
 	selector: 'inventoryfilters',
 	templateUrl: './components/inventoryfilters/inventoryfilters.html',
 	styleUrls: ['./components/inventoryfilters/inventoryfilters.css'],
 	directives: [NgFor, NgClass],
-	inputs: ['bbbb']
+	inputs: ['aaaa']
 })
 export class InventoryFiltersComponent {
 }
